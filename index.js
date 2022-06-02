1d2a49be3c907677a55b4629269dbe687c14576b
Nafis Handoko <naffisioner@gmail.com>
2022-06-02 13:38:15 +0700

    request data sudah bisa masuk

--- index.js
+++ index.js
@@ -27,18 +27,26 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname+ '/public')));
 
+// Buat nampilin view
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/views/index.html'));
 });
-
 app.get('/gosend',function(req,res){
     res.sendFile(path.join(__dirname+'/views/gosend.html'));
 });
-
-app.post('/gosend', function(req,res){
-    res.json(req.body);
+app.get('/order',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/order.html'));
+});
+app.get('/profil',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/profil.html'));
+});
+app.get('/riwayatOrder',function(req,res){
+    res.sendFile(path.join(__dirname+'/views/riwayatOrder.html'));
 });
 
+
+// CRUD masuk ke controller
+app.post('/gosend', saveGosend);
 // app.use('/', route);
 
 app.listen('3000',()=> console.log('server Running at port: 3000'));
